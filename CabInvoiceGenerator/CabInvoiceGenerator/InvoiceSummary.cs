ebb4be43bdffda2f40993ef6914a1e5ac3370399
ShrutiBelsare27 <shrutibelsare27@gmail.com>
2021-12-04 00:06:45 +0530

    Added: Calculate aggregate total for multiple rides

---
+++ CabInvoiceGenerator/CabInvoiceGenerator/InvoiceSummary.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CabInvoiceGenerator
+{
+    public class InvoiceSummary
+    {
+        private int numberOfRides;
+        private double totalFare;
+        private double avrageFare;
+
+        
+        public InvoiceSummary(int numberOfRides, double totalFare)
+        {
+            this.numberOfRides = numberOfRides;
+            this.totalFare = totalFare;
+            this.avrageFare = this.totalFare / this.numberOfRides;
+        }
+
+        /// <summary>
+        /// Determines whether the specified <see cref="System.Object" />, is equal to this instance.
+        /// </summary>
+        /// <param name="obj">The <see cref="System.Object" /> to compare with this instance.</param>
+        /// <returns>
+        ///   <c>true</c> if the specified <see cref="System.Object" /> is equal to this instance; otherwise, <c>false</c>.
+        /// </returns>
+        public override bool Equals(object obj)
+        {
+            if (obj == null) return false;
+            if (!(obj is InvoiceSummary)) return false;
+            InvoiceSummary inputObject = (InvoiceSummary)obj;
+            return this.numberOfRides == inputObject.numberOfRides && this.totalFare == inputObject.totalFare && this.avrageFare == inputObject.avrageFare;
+        }
+
+
+        public override int GetHashCode()
+        {
+            return this.numberOfRides.GetHashCode() ^ this.totalFare.GetHashCode() ^ this.avrageFare.GetHashCode();
+        }
+    }
+}
--- CabInvoiceGenerator/CabInvoiceGenratorTest/UnitTest1.cs
+++ CabInvoiceGenerator/CabInvoiceGenratorTest/UnitTest1.cs
@@ -7,9 +7,9 @@ namespace CabInvoiceGenratorTest
     {
         InvoiceGenerator invoiceGenerator = null;
 
-        /// <summary>
+        
         /// Test case 1 Given the distance and time when invoice generator then should return total fare.
-        /// </summary>
+       
         [Test]
         public void GivenDistanceAndTime_WhenInvoiceGenerator_ThenShouldReturnTotalFare()
         {
@@ -19,6 +19,18 @@ namespace CabInvoiceGenratorTest
             double fare = invoiceGenerator.CalculateFare(distance, time);
             double expected = 25;
             Assert.AreEqual(expected, fare);
+        }
+       
+        /// Test case for UC 2 Givens the multiple rides when invoice generator then should return invoice summary.
+       
+        [Test]
+        public void GivenMultipleRides_WhenInvoiceGenerator_thenShouldReturnInvoiceSummary()
+        {
+            invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+            Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1) };
+            InvoiceSummary invoiceSummary = invoiceGenerator.CalculateFare(rides);
+            InvoiceSummary expectedSummary = new InvoiceSummary(2, 30.0);
+            Assert.AreEqual(expectedSummary, invoiceSummary);
         }
     }
 }
